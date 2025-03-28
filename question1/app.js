940fa146e75e687d73cf7ed63b8ca8106384825c
Kamran Mohammed <kamranabdulbasith@gmail.com>
2025-03-28 14:40:08 +0530

    make basic logic

--- question1/app.js
+++ question1/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const axios = require("axios");
 const numberRouter = require("./routes/numberRouter");
 
 const app = express();
--- question1/controllers/numberController.js
+++ question1/controllers/numberController.js
@@ -1,6 +1,54 @@
-exports.calculate = (req, res, next) => {
+const axios = require("axios");
+
+exports.calculate = async (req, res, next) => {
+  const endpoints = {
+    p: "primes",
+    f: "fibo",
+    r: "rand",
+    e: "even",
+  };
+
+  const response1 = await axios.post("http://20.244.56.144/test/auth", {
+    companyName: "Affordmed",
+    clientID: "56083498-5cb8-4c5c-9742-c8346168bafc",
+    clientSecret: "aNVzswwLBLalAXjd",
+    ownerName: "Kamran",
+    ownerEmail: "kamranabdulbasith@gmail.com",
+    rollNo: "22911A05A1",
+  });
+
+  const token = response1.data.access_token;
+
+  const response = await axios.get(
+    `http://20.244.56.144/test/${endpoints[req.params.id]}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+  const numbers = response.data.numbers;
+  console.log(numbers);
+
+  const uniqueNumbers = [];
+  let sum = 0;
+
+  for (let num of numbers) {
+    if (!uniqueNumbers.includes(num)) {
+      uniqueNumbers.push(num);
+      sum += num;
+    }
+  }
+
+  const avg = sum / uniqueNumbers.length;
+
+  console.log(avg);
+
   res.status(200).json({
-    status: "success",
-    id: req.params.id,
+    // status: "success",
+    // id: req.params.id,
+    numbers: uniqueNumbers,
+    avg,
   });
 };
